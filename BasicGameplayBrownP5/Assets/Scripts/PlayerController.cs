b2e6e2c30439b62dd585263d9df131b80ed2a81c
Kayden Brown <145067189+Buried7564@users.noreply.github.com>
2024-03-26 13:42:29 -0700

    hvuifdd

--- BasicGameplayBrownP5/Assets/Scripts/PlayerController.cs
+++ BasicGameplayBrownP5/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject projectilePrefab;
     public float horizontalInput;
+    private float verticalInput; 
     public float speed = 10.0f;
     public float xRange = 10;
+    public float zMin;
+    public float zMax;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,9 +24,10 @@ public class PlayerController : MonoBehaviour
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
-
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput *  Time.deltaTime * speed);
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         if (transform.position.x < -10)
         {
             transform.position = new Vector3(-10, transform.position.y, transform.position.z);
@@ -31,6 +35,14 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > 10)
         {
             transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        }
+        if (transform.position.y < zMin)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMin);
+        }
+        if (transform.position.y > zMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMax);
         }
     }
 }
--- BasicGameplayBrownP5/Assets/Scripts/SpawnManager.cs
+++ BasicGameplayBrownP5/Assets/Scripts/SpawnManager.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    private float spawnRangeX = 20;
+    private float spawnRangeX = 10;
     private float spawnPosZ = 20;
     public GameObject[] animalPrefabs;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+    public GameObject[] animals;
+    public GameObject[] SideAnimals;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        InvokeRepeating("SpawnAnimalLeft", startDelay, spawnInterval);
+        InvokeRepeating("SpawnAnimalRight", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
@@ -18,9 +24,28 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            SpawnRandomAnimal();
         }
+    }
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        
+    }
+    void SpawnAnimalLeft()
+    {
+        Vector3 spawnPos = new Vector3(-10, 0, Random.Range(5,15));
+        int animalIndex = Random.Range(0, animals.Length);
+        Instantiate(animals[animalIndex], spawnPos, Quaternion.LookRotation(Vector3.back));
+
+    }
+    void SpawnAnimalRight()
+    {
+        Vector3 spawnPos = new Vector3(10, 0, Random.Range(5, 15));
+        int animalIndex = Random.Range(0, animals.Length);
+        Instantiate(animals[animalIndex], spawnPos, Quaternion.LookRotation(Vector3.back));
+
     }
 }
